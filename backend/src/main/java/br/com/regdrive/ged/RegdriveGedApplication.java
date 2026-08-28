e6980b664f9ee605a02921710f6a7b8a76cb98fa
Joao <joao.v.arbegaus@gmail.com>
2026-08-28 11:40:49 -0300

    chore: inicializa projeto

---
+++ backend/src/main/java/br/com/regdrive/ged/RegdriveGedApplication.java
@@ -0,0 +1,13 @@
+package br.com.regdrive.ged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegdriveGedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegdriveGedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/regdrive/ged/RegdriveGedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.regdrive.ged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegdriveGedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
